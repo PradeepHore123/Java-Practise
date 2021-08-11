603f2e22c56341f70d2d43e0312bebf375910b04
PradeepHore123 <pradeephore2@gmail.com>
2021-08-11 06:45:13 +0000

    Adding static import

---
+++ src/com/tcs/practice/FileReader.java
@@ -0,0 +1,11 @@
+package com.tcs.practice;
+import java.util.Arrays;//normal import
+import static java.util.Arrays.*;//Static import
+public class FileReader {
+	public static void main (String[] args) {
+		int[] numbers={1,2,3,47,8};
+		sort(numbers);//works with static import
+		Arrays.sort(numbers);//works with static import
+		
+}
+}
