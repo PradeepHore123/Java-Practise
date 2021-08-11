77d050bf81b55a473f188175557ba9f569f505cc
PradeepHore123 <pradeephore2@gmail.com>
2021-08-11 11:37:18 +0000

    string builder

---
+++ src/com/tcs/entity/StringbufferDemo.java
@@ -0,0 +1,8 @@
+package com.tcs.entity;
+
+public class StringbufferDemo {
+	String literal="TCS";//Stringpool
+	String newKeyword=new String("TCS");//immtable
+	StringBuilder  strBuilder=new StringBuilder("TCS");
+	StringBuffer strBuffer=new StringBuffer("TCS");
+}
