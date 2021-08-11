a5853bdf0c9137a4825b0af90b94ce2ac041250f
PradeepHore123 <pradeephore2@gmail.com>
2021-08-11 06:33:19 +0000

    Adding statickeyword example

---
+++ src/com/tcs/accessmodifier/A.java
@@ -0,0 +1,6 @@
+package com.tcs.accessmodifier;
+
+public class A {
+	protected int age=10;
+	
+}
---
+++ src/com/tcs/accessmodifier/B.java
@@ -0,0 +1,8 @@
+package com.tcs.accessmodifier;
+
+public class B {
+	public static void main(String[] args) {
+		A a=new A();
+		System.out.println(a.age);//IF both are there in same package 
+	}
+}
