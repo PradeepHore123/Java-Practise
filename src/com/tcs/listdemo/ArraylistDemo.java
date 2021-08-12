3a2c19309b0e0d72b85d232abbf87e227604cc95
PradeepHore123 <pradeephore2@gmail.com>
2021-08-12 06:14:44 +0000

    lambda and function programming

--- src/com/tcs/listdemo/ArraylistDemo.java
+++ src/com/tcs/listdemo/ArraylistDemo.java
@@ -6,25 +6,26 @@ import java.util.List;
 
 public class ArraylistDemo {
 	public static void main(String[] args) {
-		createList();
-	}
-	private static List<Integer>createList() {
-		// TODO Auto-generated method stub
-		List<Integer> numbers=new ArrayList<Integer>();
-		numbers.add(1);
+		List<Integer> numbers = createList();
+		System.out.println(numbers);
 		numbers.sort(new Comparator<Integer>() {
-
 			@Override
 			public int compare(Integer o1, Integer o2) {
-				// TODO Auto-generated method stub
-				return o1 -o2;
+				return o2 - o1;
 			}
 		});
-			System.out.println(numbers);
-		for(int count=0;count<20;count++) {
+		System.out.println("after sorting=" + numbers);
+	}
+
+	private static List<Integer> createList() {
+		List<Integer> numbers = new ArrayList<Integer>();// Generics
+		numbers.add(1);// autoboxing
+//		number.add("Ram");//string is not allowed
+//		System.out.println(numbers.get(0));
+
+		for (int count = 0; count < 20; count++) {
 			numbers.add(count);
 		}
 		return numbers;
 	}
-	
 }
